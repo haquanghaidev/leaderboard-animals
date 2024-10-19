fcc4822efc138d9b087645a11fa644331249b9d5
hqhai <quanghai@atlantic.edu.vn>
2024-10-19 17:10:05 +0700

    dd

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? 'leaderboard' // Thay <repository-name> bằng tên repository của bạn
+      ? 'leaderboard-amimals' // Thay <repository-name> bằng tên repository của bạn
       : '/'
   }
   
